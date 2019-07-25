124b2276b2af232918d696cefcaf1ed0494aa318
StationToStation <anastasgarbuz@gmail.com>
2019-07-25 13:37:30 +0300

    feat: shows lists as in the tutorial

--- src/webparts/agarbList/AgarbListWebPart.ts
+++ src/webparts/agarbList/AgarbListWebPart.ts
@@ -6,8 +6,19 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
+import styles from './components/AgarbList.module.scss';
 
 import * as strings from 'AgarbListWebPartStrings';
+import MockHttpClient from './MockHttpClient';
+import {
+  SPHttpClient,
+  SPHttpClientResponse   
+} from '@microsoft/sp-http';
+import {
+  Environment,
+  EnvironmentType
+} from '@microsoft/sp-core-library';
+
 import AgarbList from './components/AgarbList';
 import { IAgarbListProps } from './components/IAgarbListProps';
 
@@ -15,17 +26,91 @@ export interface IAgarbListWebPartProps {
   description: string;
 }
 
+export interface ISPLists {
+  value: ISPList[];
+}
+
+export interface ISPList {
+  Title: string;
+  Id: string;
+}
+
 export default class AgarbListWebPart extends BaseClientSideWebPart<IAgarbListWebPartProps> {
 
   public render(): void {
-    const element: React.ReactElement<IAgarbListProps > = React.createElement(
-      AgarbList,
-      {
-        description: this.properties.description
-      }
-    );
+    this.domElement.innerHTML = `
+  <div class="${ styles.agarbList }">
+    <div class="${ styles.container }">
+      <div class="${ styles.row }">
+        <div class="${ styles.column }">
+          <span class="${ styles.title }">Welcome to SharePoint!</span>
+          <p class="${ styles.subTitle }">Customize SharePoint experiences using web parts.</p>
+          <p class="${ styles.description }">${escape(this.properties.description)}</p>
+          <p class="${ styles.description }">Loading from ${escape(this.context.pageContext.web.title)}</p>
+          <a href="https://aka.ms/spfx" class="${ styles.button }">
+            <span class="${ styles.label }">Learn more</span>
+          </a>
+        </div>
+      </div>
+      <div id="spListContainer" />
+    </div>
+  </div>`;
 
-    ReactDom.render(element, this.domElement);
+  this._renderListAsync();
+    // const element: React.ReactElement<IAgarbListProps > = React.createElement(
+    //   AgarbList,
+    //   {
+    //     description: this.properties.description
+    //   }
+    // );
+
+    // ReactDom.render(element, this.domElement);
+  }
+
+  private _getMockListData(): Promise<ISPLists> {
+    return MockHttpClient.get()
+      .then((data: ISPList[]) => {
+        var listData: ISPLists = { value: data };
+        return listData;
+      }) as Promise<ISPLists>;
+  }
+
+  private _getListData(): Promise<ISPLists> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      });
+  }
+
+  private _renderList(items: ISPList[]): void {
+    let html: string = '';
+    items.forEach((item: ISPList) => {
+      html += `
+    <ul class="${styles.list}">
+      <li class="${styles.listItem}">
+        <span class="ms-font-l">${item.Title}</span>
+      </li>
+    </ul>`;
+    });
+  
+    const listContainer: Element = this.domElement.querySelector('#spListContainer');
+    listContainer.innerHTML = html;
+  }
+
+  private _renderListAsync(): void {
+    // Local environment
+    if (Environment.type === EnvironmentType.Local) {
+      this._getMockListData().then((response) => {
+        this._renderList(response.value);
+      });
+    }
+    else if (Environment.type == EnvironmentType.SharePoint || 
+              Environment.type == EnvironmentType.ClassicSharePoint) {
+      this._getListData()
+        .then((response) => {
+          this._renderList(response.value);
+        });
+    }
   }
 
   protected onDispose(): void {
